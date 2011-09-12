35effccf29739ba79e1d84f01347cf6f75eef08c
Dave Moore <dcmoore@Dave-Moores-MacBook-Pro.local>
2011-09-12 09:24:11 -0500

    Cleared repo

--- config/development/repl_init.clj
+++
@@ -1,5 +0,0 @@
-(use 'gaeshi.tsukuri.environment)
-
-(setup-environment "specljweb-development")
-
-(println "specljweb-development env has been loaded")
--- src/specljweb/view/view_helpers.clj
+++
@@ -1,6 +0,0 @@
-(ns specljweb.view.view-helpers
-  "Put helper functions for views in this namespace."
-  (:use
-    [gaeshi.views :only (render-partial *view-context*)]
-    [hiccup.page-helpers]
-    [hiccup.form-helpers]))
