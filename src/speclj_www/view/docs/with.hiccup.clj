aedf27ca8d65860b57c8430e3a2c4bf3e0b626bf
Dave Moore <dcmoore@Dave-Moores-MacBook-Pro.local>
2011-09-30 15:21:45 -0500

    Made some css fixes.

--- src/speclj_www/view/docs/with.hiccup.clj
+++ src/speclj_www/view/docs/with.hiccup.clj
@@ -1,9 +1,9 @@
 [:h3 "with"]
 
-[:p "Declares a reference-able symbol that will be lazily evaluated once per characteristic of the containing describe scope. The body may contain any forms, the last of which will be the value of the dereferenced symbol."]
+[:div {:id "comp_box"}
+  [:p "Declares a reference-able symbol that will be lazily evaluated once per characteristic of the containing describe scope. The body may contain any forms, the last of which will be the value of the dereferenced symbol."]
 
-[:code "
-<pre>(with nice-format (java.text.DecimalFormat. \"0.00000\"))
-
-(it \"formats numbers nicely\"
-  (should= \"3.14159\" (.format @nice-format Math/PI)))</pre>"]
+  [:code "(with nice-format (java.text.DecimalFormat. \"0.00000\"))<br />
+    <br />
+    (it \"formats numbers nicely\"<br />
+      &nbsp;&nbsp;(should= \"3.14159\" (.format @nice-format Math/PI)))<br />"]]
