65a89c8ce163b518a56a5bc910ece638aee3195f
Dave Moore <dcmoore@Dave-Moores-MacBook-Pro.local>
2011-11-09 15:28:34 -0600

    Tiny fix

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject speclj_www "0.0.1"
   :description "A website deployable to AppEngine"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [joodo "0.6.0-SNAPSHOT"]]
+                 [joodo "0.6.0-SNAPSHOT"]
+                 [speclj "1.4.0"]]
   :dev-dependencies [[speclj "1.4.0"]]
   :test-path "spec/"
   :java-source-path "src/"
