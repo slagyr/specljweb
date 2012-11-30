ae827682efb58c8ba7a8d5ee2dddd265dff8e78c
Ryan Verner <ryanzverner@gmail.com>
2012-11-29 22:48:16 -0600

    Elaborated on the description of tags and modified the code example

--- src/speclj_www/view/docs/tags.hiccup.clj
+++ src/speclj_www/view/docs/tags.hiccup.clj
@@ -1,6 +1,17 @@
 [:h3 "tags"]
 
 [:div {:id "comp_box"}
-  [:p "Add tags to the containing context.  All values passed will be converted into keywords.  Contexts can be filtered at runtime by their tags."]
+  [:p "Add tags to wrap related tests.  Values passed will be converted into keywords."]
 
-  [:code "(tags :one :two)"]]
+  [:code "(describe \"I/O Tests\"<br />
+    &nbsp;&nbsp;(tags :unit)
+    &nbsp;&nbsp;&nbsp;&nbsp; ;tests with the unit tag will be contained here
+    )"]
+
+  [:p "Tags can be filtered at runtime using the -t flag. In the example below, only the tests wrapped in the unit tag will be run."]
+
+  [:code "lein spec -t unit"]
+
+  [:p "To specify certain tests to exclude, prepend the tag name with ~."]
+
+  [:code "lein spec -t ~unit"]]
