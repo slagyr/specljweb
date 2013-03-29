5e32df66d693c5c4c7bb50472c79cbff20da388c
Sandro Padin <sandropadin@gmail.com>
2013-03-29 10:57:33 -0500

    Project uses Leiningen 2 and up-to-date deps.
    Updating Speclj to 2.5.0
    Updating project.clj for Lein 2
    Updating dependency versions and related files.
    Install instructions for Leiningen 2

--- project.clj
+++ project.clj
@@ -1,10 +1,18 @@
 (defproject speclj_www "0.0.1"
   :description "A website deployable to AppEngine"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [joodo "0.6.0-SNAPSHOT"]
-                 [speclj "1.4.0"]]
-  :dev-dependencies [[speclj "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [joodo "1.1.2"]]
+
+  ; lein 1.x
+  :dev-dependencies [[speclj "2.5.0"]]
   :test-path "spec/"
+
+   ;lein 2.x
+  :profiles {:dev {:dependencies [[speclj "2.5.0"]]}}
+  :plugins [[speclj "2.5.0"]
+            [joodo/lein-joodo "1.1.2"]]
+  :test-paths ["spec"]
+
   :java-source-path "src/"
   :repl-init-script "config/development/repl_init.clj"
   :joodo-core-namespace speclj_www.core)
