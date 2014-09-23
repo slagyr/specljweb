2a434c28926e69535a0c3bf4118e0ca7e067e388
Luke Winikates <luke.winikates@gmail.com>
2014-09-22 22:45:38 -0700

    update documentation to use the latest speclj version

--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
   :description "Speclj's Documentation Website"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [joodo "2.0.0"]
-                 [speclj "2.9.1"]]
-  :dev-dependencies [[speclj "2.9.1"]]
+                 [speclj "3.1.0"]]
+  :dev-dependencies [[speclj "3.1.0"]]
   :test-paths ["spec/"]
   :java-source-paths ["src/"]
   :min-lein-version "2.0.0"
-  :plugins [[speclj "2.9.1"]
+  :plugins [[speclj "3.1.0"]
             [lein-ring "0.8.8"]]
   :ring {:handler speclj_www.core/app}
   )
